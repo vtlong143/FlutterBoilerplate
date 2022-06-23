1c17d0326079b3d20396276d6d6359bfc5fbf1fe
Vu Long <longvt2@flextech.vn>
2022-06-23 12:32:13 +0700

    Initial commit

---
+++ lib/theme/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const colorTransparent = Color(0x00000000);
+const colorWhite = Color(0xffffffff);
+const colorBlack = Color(0xff000000);
---
+++ lib/theme/text_styles.dart
@@ -0,0 +1,10 @@
+import 'package:boilerplate/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+final typoBold24 = TextStyle(
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w700,
+  fontSize: 24,
+  letterSpacing: 1.02,
+  color: colorBlack,
+);
